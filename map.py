8be0c7eb199ec93193781d29e8d20812e27be2a8
chao.jiang <14317048@qq.com>
2022-09-23 16:44:35 +0800

    生成网格和障碍物体

--- map.py
+++ map.py
@@ -1,41 +1,88 @@
-import turtle
+from dis import dis
+import turtle,random,string
 sc= turtle.Screen()
 pen = turtle.Turtle()
+wn=turtle.Screen()
 #小格子宽度
 distance =50
 
+
+
+
 #正方形
-def draw():
+def draw(x,y,dist):
+    drawFill(x,y,dist,'#ffffff')
+
+#颜色填充 green or #ffffff
+def drawFill(x,y,dist,fillcolor):
+    pen.begin_fill()
+    pen.fillcolor(fillcolor)
+    pen.up()
+    pen.goto(x,y)
+    pen.down()
     for i in range(4):
         #向当前画笔方向移动distance像素长度
-        pen.forward(distance)
+        pen.forward(dist)
         pen.left(90)
-    pen.forward(distance)
+    pen.end_fill()            
 
 def draws(x,y,size):
-    for i in range(size):
-        draw()
-        pen.goto(-300+i,y)
-        pen.setx(distance*i)
-        print (pen.position)
-        draw()
+      nextX =x;
+      nextY= y;
+      for yIndex in range(size):
+        for xIndex in range(size):
+           draw(nextX,nextY,distance)
+           nextX = x + distance* (xIndex+1)
+        nextX = x   
+        nextY = y + distance* (yIndex+1) 
 
 
+
+def drawFast(x,y,size):
+    nextX =x;
+    nextY= y;
+    for yIndex in range(size+1):
+        pen.up()
+        pen.goto(nextX,nextY)
+        pen.down()
+        pen.forward(distance*size+1)
+        nextY = y + distance* (yIndex+1)
+    nextX =x;
+    nextY= y;
+    pen.left(90)
+    for xIndex in range(size+1):
+        pen.up()
+        pen.goto(nextX,nextY)
+        pen.down()   
+        pen.forward(distance*size+1)
+        nextX = x + distance* (xIndex+1)
+    pen.right(90)  
+    
 def turn(x, y):
 # 打印当前坐标
    print('x=', x, 'y=', y)
-# 打印当前方向
-   print(turtle.heading())
 
+
+def setObstacles(count):
+    for i in range(count):
+        x = random.randint(1,11)*distance
+        y = random.randint(1,11)*distance
+        drawFill(x-300,y-300,distance,'black')
+
+def initStartEnd():
+    pen.speed(0)
+    drawFill(-300,-300,distance,'green')
+    drawFill(200,250,distance,'red')
 
 if __name__ == "__main__" :
+    wn.screensize()
+    wn.setup(width = 1.0, height = 1.0)
     turtle.onclick(turn)
-    pen.speed(1)
-    sc.setup(600,600)
-    #pen.up()
-    pen.goto(-300,-300)
-    #pen.down()
-    draws(-300,-300,8)
+    pen.speed(0)
+    sc.setup(650,650)
+    drawFast(-300,-300,12)
+    initStartEnd()
+    setObstacles(30)
     pen.hideturtle()
 turtle.done()
 
